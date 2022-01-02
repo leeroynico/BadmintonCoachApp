a57745771dbc1f8ff89a1a9f1f8f4d9f5db92aa1
leeroynico <leeroynicolas62@gmail.com>
2022-01-02 22:32:39 +0100

    init commit

---
+++ components/buttons/StartButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StartButton() {
+  return <div>Je suis le bouton </div>;
+}
+
+export default StartButton;
---
+++ components/style/Style.js
@@ -0,0 +1,6 @@
+export const color = {
+  blue: "#537d8d",
+  red: "#db3a34",
+  grey: "#1F1E1D",
+  black: "#0f0f0e",
+};
